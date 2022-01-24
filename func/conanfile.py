244e71c5ec59a023e79f797547ff8182306b696c
Chris Marsh <chrismarsh.c2@gmail.com>
2022-01-24 10:28:59 -0600

    update func build to have a conandata.yml so it is correctly built on the CI

--- func/conanfile.py
+++ func/conanfile.py
@@ -1,25 +1,27 @@
 from conans import ConanFile, CMake, tools
 
 
-class CgalConan(ConanFile):
+class FuncConan(ConanFile):
     name = "func"
     license = "GPL/LGPL"
     url = "https://github.com/uofs-simlab/func"
     description = "(Function Comparator) is a C++ tool for quickly profiling the performance of various different abstracted implementations of mathematical function evaluations"
-    no_copy_source = True
+    # no_copy_source = True
     settings = "os", "compiler", "build_type", "arch"
 
     generators = "cmake_find_package"
 
+    _source_folder='func'
+
     def source(self):
-        git = tools.Git()
-        git.clone("https://github.com/uofs-simlab/func") 
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename("func-master", self._folder )
 
         tools.replace_in_file("src/CMakeLists.txt", "target_link_libraries(func PUBLIC ","target_link_libraries(func PUBLIC Boost::Boost")
 
 
     def requirements(self):
-        self.requires("boost/[>=1.67]@CHM/stable")
+        self.requires("boost/[>=1.75.0]@CHM/stable")
 
 
     def configure_cmake(self):
