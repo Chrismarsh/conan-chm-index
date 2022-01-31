c1ee36375bd54ee23ba78bc2dd253901c37398b2
Chris Marsh <chrismarsh.c2@gmail.com>
2022-01-31 12:24:33 -0600

    [netcdf-cxx] fix runpaths

--- netcdf-cxx/conanfile.py
+++ netcdf-cxx/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 import os 
+from fnmatch import fnmatch
 
 class NetcdfcConan(ConanFile):
     name = "netcdf-cxx"
@@ -44,7 +45,18 @@ class NetcdfcConan(ConanFile):
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
 
         if tools.os_info.is_macos:
-            cmake.definitions["CMAKE_INSTALL_NAME_DIR"] = "@rpath"
+            cmake.definitions["CMAKE_INSTALL_RPATH"] = "@executable_path"
+        else:
+            #this seems to be ignored??
+            cmake.definitions["CMAKE_INSTALL_RPATH"] = "\$ORIGIN"
+
+        cmake.definitions["CMAKE_BUILD_WITH_INSTALL_RPATH"] = True
+
+        # ensure we don't set the install_name to be the fully qualified path otherwise
+        # it won't be relocatable
+        if tools.os_info.is_macos:
+            cmake.definitions["CMAKE_INSTALL_NAME_DIR"] = '@rpath' 
+
         cmake.configure(source_folder="netcdf-cxx4")
         return cmake
 
@@ -56,10 +68,21 @@ class NetcdfcConan(ConanFile):
         cmake = self.configure_cmake()
         cmake.install()
 
-        # if tools.os_info.is_linux:
-        #     with tools.chdir(self.package_folder):
-        #         cmd = "patchelf --remove-rpath lib/libnetcdf-cxx4.so"
-        #         os.system(cmd)
+        for path, subdirs, names in os.walk(os.path.join(self.package_folder, 'lib')):
+            for name in names:
+
+                if fnmatch(name, '*.dylib*'):
+                    so_file = os.path.join(path, name)
+
+                    cmd = "install_name_tool -add_rpath @executable_path {0}".format(so_file)
+                    os.system(cmd)
+
+                if fnmatch(name, '*.so*'):
+                    so_file = os.path.join(path, name)
+
+                    cmd = "patchelf --set-rpath \$ORIGIN {0}".format(so_file)
+                    os.system(cmd)
+
 
     def package_info(self):
         self.cpp_info.libs = ["netcdf-cxx4"]
