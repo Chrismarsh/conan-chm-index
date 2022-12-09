81ea98c864d123065108fcae710bf66c2d6ab979
Chris Marsh <chrismarsh.c2@gmail.com>
2022-12-09 16:10:20 -0600

    Don't build WN until I can figure out actions/runner-images/issues/5555

--- windninja/build.py
+++ windninja/build.py
@@ -11,6 +11,9 @@ if __name__ == "__main__":
     builder.add_common_builds(pure_c=False, shared_option_name=False)
     builder.remove_build_if(lambda build: build.settings["compiler.libcxx"] == "libstdc++") # old ABI
 
+    #building in the CI disable the omp build of Windninja because of weirndess with the arm64/64 libomp
+    builder.update_build_if(lambda build: os.environ['CI'], new_options={'windninja:openmp': False})
+
 
     builder.run()
 
